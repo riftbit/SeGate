f2eb907dc25b899282376f89eac04816c007b7b9
Riftbit ErgoZ <ergozru@gmail.com>
2018-03-14 19:42:30 +0300

    fix build.bat

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 set CGO_ENABLED=0
 set GOOS=linux
 set GOARCH=amd64
+set SGVERS=v1.0.1
 
 FOR /F "tokens=*" %%a in ('git rev-parse --short HEAD') do SET SGBUILD=%%a
 
@@ -8,4 +9,4 @@ set dd=%date:~0,2%
 set mm=%date:~3,2%
 set yyyy=%date:~6,4%
 
-go build -a -installsuffix cgo -ldflags "-s -w -X 'main.version=v1.0.1' -X 'main.build=%SGBUILD%' -X 'main.buildDate=%dd%-%mm%-%yyyy%'" ./
+go build -a -installsuffix cgo -ldflags "-s -w -X 'main.version=%SGVERS%' -X 'main.build=%SGBUILD%' -X 'main.buildDate=%dd%-%mm%-%yyyy%'" ./
